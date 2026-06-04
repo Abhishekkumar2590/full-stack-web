7af64d60e2c1046c5b690d1520413e49f08df9a9
Abhishekkumar2590 <abhishek993526@gmail.com>
2026-06-04 13:33:10 +0530

    add backend url

--- frontedns--/src/api.js
+++ frontedns--/src/api.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants"
+
+const API_URL = "/choreo-apis/backend-1338620691:8000"
  
 const  api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : API_URL,
 
 
-})
+});
 
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem(ACCESS_TOKEN);
